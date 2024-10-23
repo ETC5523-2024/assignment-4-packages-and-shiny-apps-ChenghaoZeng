579b5b9a6b1e0572e0c830303ce439d88f615d8a
Chenghao Zeng <czen0003@student.monash.edu>
2024-10-24 00:26:56 +1100

    add data desc

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Valentine's Day Consumer Spending Data from 2010 to 2022
+#'
+#' A dataset containing historical consumer spending trends on Valentine's Day in the United States across multiple categories, from the years 2010 to 2022.
+#'
+#' @format ## 'historical_spending'
+#' A data frame with 13 rows and 10 columns:
+#' \describe{
+#'   \item{Year}{Year of recorded spending (2010-2022).}
+#'   \item{PercentCelebrating}{Percentage of people celebrating Valentine's Day in the given year.}
+#'   \item{PerPerson}{Average amount spent per person in USD.}
+#'   \item{Candy}{Spending on candy in USD.}
+#'   \item{Flowers}{Spending on flowers in USD.}
+#'   \item{Jewelry}{Spending on jewelry in USD.}
+#'   \item{GreetingCards}{Spending on greeting cards in USD.}
+#'   \item{EveningOut}{Spending on evening outings in USD.}
+#'   \item{Clothing}{Spending on clothing in USD.}
+#'   \item{GiftCards}{Spending on gift cards in USD.}
+#' }
+#' @source <https://github.com/rfordatascience/tidytuesday/tree/master/data/2024/2024-02-13>
